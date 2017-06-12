095f96034aafa790afe344ae39a37924fd9755fe
yenyen-PC\yenyen <lkimlyen2912@gmail.com>
2017-06-12 11:24:04 +0700

    up hình, add database helper

--- DuoiHinhBatChuDemo/app/src/main/java/com/example/yenyen/duoihinhbatchudemo/CauHoi.java
+++ DuoiHinhBatChuDemo/app/src/main/java/com/example/yenyen/duoihinhbatchudemo/CauHoi.java
@@ -5,4 +5,57 @@ package com.example.yenyen.duoihinhbatchudemo;
  */
 
 class CauHoi {
+    int id;
+    String imagePath, description, shortAnswer, fullAnswer;
+
+    public CauHoi() {
+    }
+
+    public CauHoi(int id, String imagePath, String description, String shortAnswer, String fullAnswer) {
+        this.id = id;
+        this.imagePath = imagePath;
+        this.description = description;
+        this.shortAnswer = shortAnswer;
+        this.fullAnswer = fullAnswer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getShortAnswer() {
+        return shortAnswer;
+    }
+
+    public void setShortAnswer(String shortAnswer) {
+        this.shortAnswer = shortAnswer;
+    }
+
+    public String getFullAnswer() {
+        return fullAnswer;
+    }
+
+    public void setFullAnswer(String fullAnswer) {
+        this.fullAnswer = fullAnswer;
+    }
 }
